d541caa6c6f6adfe016532ac6f30915a3f84fe12
prashantrahul141 <prashantrahul141@protonmail.com>
2025-07-02 17:42:20 +0530

    feat: log to file if release build
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,12 @@
 #include "src/config.hpp"
 
 int main(int argc, char *argv[]) {
-  nhlog_init(APPLICATION_DEBUG_LEVEL, NULL);
+#ifdef DEBUD_BUILD
+  void *fd = NULL;
+#else
+  void *fd = fopen("logs.txt", "w");
+#endif
+  nhlog_init(APPLICATION_DEBUG_LEVEL, (FILE *)fd);
   App app = App();
   return app.run();
 }
