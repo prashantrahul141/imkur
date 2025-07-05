52542f4431a203d37b263cb67e21e135d5abcab9
prashantrahul141 <prashantrahul141@protonmail.com>
2025-07-05 15:51:10 +0530

    fix: disables warnings from std_image* files
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- thirdparty/stb_image.h
+++ thirdparty/stb_image.h
@@ -1,3 +1,6 @@
+// this disables all warnings from this file
+#pragma clang system_header
+
 /* stb_image - v2.30 - public domain image loader - http://nothings.org/stb
                                   no warranty implied; use at your own risk
 
